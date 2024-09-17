2ea66a919c44b1ecddf39482f800f112226cdb95
Nathan Vieira <nathanrodriguesvieira.contato@gmail.com>
2024-09-17 08:39:09 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import apiService from './apiService';
+
+function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/apiService.jsx
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+const api = axios.create({ baseURL: ' https://api.npoint.io/c6d3aeb4d8c1f9de4838', headers: 'Accept':'application/json','content-type:' 'aplication.json' })
