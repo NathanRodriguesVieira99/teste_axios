b9f515d3c7a6499f10d5bba07ce2ecadb4cc4e32
Nathan Vieira <nathanrodriguesvieira.contato@gmail.com>
2024-09-17 10:00:06 -0300

    commit final

--- src/apiService.jsx
+++ src/apiService.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import apiKey from './apiKey';
 
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
@@ -15,7 +15,7 @@ export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 const fetchPopularMovies = async () => {
   try {
-    const response = await api.get("/movie/popular?api_key=3a1475ec3551ecc5efc1192090e961e8");
+    const response = await api.get(`/movie/popular?api_key=${apiKey}`);
     return response.data.results;
   } catch (error) {
     console.error('Erro ao buscar filmes populares', error);
