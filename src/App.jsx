86a971ceaab17e4a3204bc89b44966555eacb4e4
Nathan Vieira <nathanrodriguesvieira.contato@gmail.com>
2024-09-17 09:36:14 -0300

    código pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
-import apiService from './apiService';
+import React, { useState, useEffect } from 'react';
+import fetchPopularMovies, { IMAGE_BASE_URL } from './apiService';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const response = await fetchPopularMovies();
+        setMovies(response.slice(0, 10));
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getMovies();
+  }, []);
+
+  if (loading) return <p>Carregando...</p>;
+  if (error) return <p>Erro: {error}</p>;
 
   return (
     <>
-
+      <h1>Filmes Populares</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <h2>{movie.title}</h2>
+            {movie.poster_path && (
+              <img
+                src={`${IMAGE_BASE_URL}${movie.poster_path}`}
+                alt={`Poster de ${movie.title}`}
+                style={{ width: '200px' }}
+              />
+            )}
+            <p>{movie.overview}</p>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/apiService.jsx
+++ src/apiService.jsx
@@ -1,3 +1,26 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: ' https://api.npoint.io/c6d3aeb4d8c1f9de4838', headers: 'Accept':'application/json','content-type:' 'aplication.json' })
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+
+const fetchPopularMovies = async () => {
+  try {
+    const response = await api.get("/movie/popular?api_key=3a1475ec3551ecc5efc1192090e961e8");
+    return response.data.results;
+  } catch (error) {
+    console.error('Erro ao buscar filmes populares', error);
+    throw error;
+  }
+};
+
+export default fetchPopularMovies;
